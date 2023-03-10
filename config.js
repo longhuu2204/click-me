6570ac65b6c5356fd089f39d97c8f2b6235b1824
longhuu2204 <long.huu.2204@gmail.com>
2023-03-10 12:16:40 +0700

    creat

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Cô giáo xinh gứi ~~';
+const giftUrl = '';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng bạn Nga';
+const contentCard = 'Chúc Nga sinh nhật tràn ngập niềm vui và những nụ cười. Mong mọi điều đẹp nhất sẽ đến với Nga trong hôm nay và cả mãi về sau ~~';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'Nga4.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
